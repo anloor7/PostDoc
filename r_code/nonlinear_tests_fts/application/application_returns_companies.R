3f4b991c3941e4089014cf56a2f1eabae247af4d
Ángel López Oriona <oriona38@hotmail.com>
2026-01-06 09:55:10 +0300

    Update application_returns_companies.R

--- r_code/nonlinear_tests_fts/application/application_returns_companies.R
+++ r_code/nonlinear_tests_fts/application/application_returns_companies.R
@@ -14,7 +14,7 @@ fdata_1 <- t(fts_1)
 
 stats_fqa_1 <- numeric()
 crit_fqa_1 <- numeric()
-p_values_fqa_1 <- numeric()
+p_values_fqa_1 <- numeric() # Vector of p-values for FQA
 
 for (i in 1 : 10) {
     
@@ -31,7 +31,7 @@ for (i in 1 : 10) {
     
 }
 
-p_values_fpc_1 <- numeric()
+p_values_fpc_1 <- numeric() # Vector of p-values for FPC
 
 for (i in 1 : 10) {
     
@@ -41,7 +41,7 @@ for (i in 1 : 10) {
     
 }
 
-p_values_facf_1 <- numeric()
+p_values_facf_1 <- numeric() # Vector of p-values for FACF
 
 for (i in 1 : 10) {
     
@@ -52,9 +52,9 @@ for (i in 1 : 10) {
 }
 
 sdo_res <- spectral_test(fdata_1, kernel = 'Parzen', bandwidth = 'adaptive', alpha = alpha)
-p_values_sdo_1 <- rep(sdo_res$p_value, 10)
+p_values_sdo_1 <- rep(sdo_res$p_value, 10) # Vector of p-values for SDO
 
-p_values_fsacf_1 <- numeric()
+p_values_fsacf_1 <- numeric() # Vector of p-values for FSACF
 
 for (i in 1 : 10) {
     
@@ -72,7 +72,7 @@ fdata_2 <- t(fts_2)
 
 stats_fqa_2 <- numeric()
 crit_fqa_2 <- numeric()
-p_values_fqa_2 <- numeric()
+p_values_fqa_2 <- numeric() # Vector of p-values for FQA
 
 for (i in 1 : 10) {
     
@@ -89,7 +89,7 @@ for (i in 1 : 10) {
     
 }
 
-p_values_fpc_2 <- numeric()
+p_values_fpc_2 <- numeric() # Vector of p-values for FPC 
 
 for (i in 1 : 10) {
     
@@ -99,7 +99,7 @@ for (i in 1 : 10) {
     
 }
 
-p_values_facf_2 <- numeric()
+p_values_facf_2 <- numeric() # Vector of p-values for FACF
 
 for (i in 1 : 10) {
     
@@ -110,9 +110,9 @@ for (i in 1 : 10) {
 }
 
 sdo_res <- spectral_test(fdata_2, kernel = 'Parzen', bandwidth = 'adaptive', alpha = alpha)
-p_values_sdo_2 <- rep(sdo_res$p_value, 10)
+p_values_sdo_2 <- rep(sdo_res$p_value, 10) # Vector of p-values for SDO
 
-p_values_fsacf_2 <- numeric()
+p_values_fsacf_2 <- numeric() # Vector of p-values for FSACF
 
 for (i in 1 : 10) {
     
@@ -130,7 +130,7 @@ fdata_3 <- t(fts_3)
 
 stats_fqa_3 <- numeric()
 crit_fqa_3 <- numeric()
-p_values_fqa_3 <- numeric()
+p_values_fqa_3 <- numeric() # Vector of p-values for FQA
 
 for (i in 1 : 10) {
     
@@ -147,7 +147,7 @@ for (i in 1 : 10) {
     
 }
 
-p_values_fpc_3 <- numeric()
+p_values_fpc_3 <- numeric() # Vector of p-values for FPC
 
 for (i in 1 : 10) {
     
@@ -157,7 +157,7 @@ for (i in 1 : 10) {
     
 }
 
-p_values_facf_3 <- numeric()
+p_values_facf_3 <- numeric() # Vector of p-values for FACF
 
 for (i in 1 : 10) {
     
@@ -168,9 +168,9 @@ for (i in 1 : 10) {
 }
 
 sdo_res <- spectral_test(fdata_3, kernel = 'Parzen', bandwidth = 'adaptive', alpha = alpha)
-p_values_sdo_3 <- rep(sdo_res$p_value, 10)
+p_values_sdo_3 <- rep(sdo_res$p_value, 10) # Vector of p-values for SDO
 
-p_values_fsacf_3 <- numeric()
+p_values_fsacf_3 <- numeric() # Vector of p-values for FSACF
 
 for (i in 1 : 10) {
     
@@ -188,7 +188,7 @@ fdata_4 <- t(fts_4)
 
 stats_fqa_4 <- numeric()
 crit_fqa_4 <- numeric()
-p_values_fqa_4 <- numeric()
+p_values_fqa_4 <- numeric() # Vector of p-values for FQA
 
 for (i in 1 : 10) {
     
@@ -205,7 +205,7 @@ for (i in 1 : 10) {
     
 }
 
-p_values_fpc_4 <- numeric()
+p_values_fpc_4 <- numeric() # Vector of p-values for FPC
 
 for (i in 1 : 10) {
     
@@ -215,7 +215,7 @@ for (i in 1 : 10) {
     
 }
 
-p_values_facf_4 <- numeric()
+p_values_facf_4 <- numeric() # Vector of p-values for FACF
 
 for (i in 1 : 10) {
     
@@ -226,9 +226,9 @@ for (i in 1 : 10) {
 }
 
 sdo_res <- spectral_test(fdata_4, kernel = 'Parzen', bandwidth = 'adaptive', alpha = alpha)
-p_values_sdo_4 <- rep(sdo_res$p_value, 10)
+p_values_sdo_4 <- rep(sdo_res$p_value, 10) # Vector of p-values for SDO
 
-p_values_fsacf_4 <- numeric()
+p_values_fsacf_4 <- numeric() # Vector of p-values for FSACF
 
 for (i in 1 : 10) {
     
@@ -246,7 +246,7 @@ fdata_5 <- t(fts_5)
 
 stats_fqa_5 <- numeric()
 crit_fqa_5 <- numeric()
-p_values_fqa_5 <- numeric()
+p_values_fqa_5 <- numeric() # Vector of p-values for FQA
 
 for (i in 1 : 10) {
     
@@ -263,7 +263,7 @@ for (i in 1 : 10) {
     
 }
 
-p_values_fpc_5 <- numeric()
+p_values_fpc_5 <- numeric() # Vector of p-values for FPC
 
 for (i in 1 : 10) {
     
@@ -273,7 +273,7 @@ for (i in 1 : 10) {
     
 }
 
-p_values_facf_5 <- numeric()
+p_values_facf_5 <- numeric() # Vector of p-values for FACF
 
 for (i in 1 : 10) {
     
@@ -284,9 +284,9 @@ for (i in 1 : 10) {
 }
 
 sdo_res <- spectral_test(fdata_5, kernel = 'Parzen', bandwidth = 'adaptive', alpha = alpha)
-p_values_sdo_5 <- rep(sdo_res$p_value, 10)
+p_values_sdo_5 <- rep(sdo_res$p_value, 10) # Vector of p-values for SDO
 
-p_values_fsacf_5 <- numeric()
+p_values_fsacf_5 <- numeric() # Vector of p-values for FSACF
 
 for (i in 1 : 10) {
     
@@ -304,7 +304,7 @@ fdata_6 <- t(fts_6)
 
 stats_fqa_6 <- numeric()
 crit_fqa_6 <- numeric()
-p_values_fqa_6 <- numeric()
+p_values_fqa_6 <- numeric() # Vector of p-values for FQA
 
 for (i in 1 : 10) {
     
@@ -321,7 +321,7 @@ for (i in 1 : 10) {
     
 }
 
-p_values_fpc_6 <- numeric()
+p_values_fpc_6 <- numeric() # Vector of p-values for FPC
 
 for (i in 1 : 10) {
     
@@ -331,7 +331,7 @@ for (i in 1 : 10) {
     
 }
 
-p_values_facf_6 <- numeric()
+p_values_facf_6 <- numeric() # Vector of p-values for FACF
 
 for (i in 1 : 10) {
     
@@ -342,9 +342,9 @@ for (i in 1 : 10) {
 }
 
 sdo_res <- spectral_test(fdata_6, kernel = 'Parzen', bandwidth = 'adaptive', alpha = alpha)
-p_values_sdo_6 <- rep(sdo_res$p_value, 10)
+p_values_sdo_6 <- rep(sdo_res$p_value, 10) # Vector of p-values for SDO
 
-p_values_fsacf_6 <- numeric()
+p_values_fsacf_6 <- numeric() # Vector of p-values for FSACF
 
 for (i in 1 : 10) {
     
