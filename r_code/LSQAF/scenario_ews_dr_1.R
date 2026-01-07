890958005d473ff35b8d60a688a50f9732f33485
Ángel López Oriona <oriona38@hotmail.com>
2026-01-07 10:39:49 +0300

    Add files via upload

---
+++ r_code/LSQAF/scenario_ews_dr_1.R
@@ -0,0 +1,41 @@
+
+series_1 <- list()
+series_2 <- list()
+series_3 <- list()
+series_length <- 200
+n_series <- 5
+K <- 3
+ground_truth <- rep(1 : K, each = n_series)
+ari <- numeric()
+trials <- 200
+ncomp <- 1
+
+
+for (j in 1 : trials) {
+  
+  for (i in 1 : n_series) {
+    
+    series_1[[i]] <- simulate_tvar_2(series_length, 1, 1.5, 0)
+    series_2[[i]] <- simulate_tvar_2(series_length, -1, 1.5, 0)
+    series_3[[i]] <- simulate_tvar_2(series_length, -1, 0.8, 0)
+    
+  }
+  
+  cluster <- c(series_1, series_2, series_3)
+  
+  features_wavelets <- lapply(cluster, ews_vector)
+  feature_matrix <- list_to_matrix(features_wavelets)
+  pca_result <- prcomp(feature_matrix, center = TRUE, scale = TRUE)
+  pc_scores <- pca_result$x[, 1:ncomp]
+  dist_matrix <- dist(pc_scores, method = "euclidean")^2
+  partition <- pam(dist_matrix, k = K)$clustering
+  ari[j] <- external_validation(partition, ground_truth)
+  
+  
+  print(j)
+  
+}
+
+mean(ari)
+
+
